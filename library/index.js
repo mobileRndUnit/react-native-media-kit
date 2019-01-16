eb0962a93d95c2b7396fa75a11077c979a2b6c45
Oleksii Kolomatskiy <felexey1@gmail.com>
2019-01-16 17:49:03 +0200

    project was added

---
+++ library/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import MediaPlayerView from './MediaPlayerView';
+import Controls from './Controls';
+
+const Video = MediaPlayerView;
+const Audio = MediaPlayerView;
+
+export {Video, Audio, Controls};
